c569396d3cea3cd97e10d373a0a5674b97339b0a
Dr Dougie <aaron.m.larsh@gmail.com>
2020-08-01 19:31:40 -0700

    posting works.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require("path");
 const dbJSON = require('../db/db.json')
 
 // ===============================================================================
@@ -9,39 +8,18 @@ const dbJSON = require('../db/db.json')
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
     return res.json(dbJSON);
-});
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+  // Create New Characters - takes in JSON input
+  app.post("/api/notes", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newNote = req.body;
 
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
+  console.log(newNote);
 
-    res.json({ ok: true });
+  dbJSON.push(newNote);
+
+  res.json(newNote);
   });
 };

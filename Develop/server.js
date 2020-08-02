12c943e2ebddf8806e7f4e13072da1f30a2ddac7
Dr Dougie <aaron.m.larsh@gmail.com>
2020-08-01 17:09:30 -0700

    pushing server.js. with routing fucntionallity.

--- Develop/server.js
+++ Develop/server.js
@@ -23,10 +23,10 @@ app.use(express.json());
 
 //routes
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
 });
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
 });
 
 // =============================================================================
